b2369b71e5beeedd89ce6dcc66f26d72f12aec84
jin10086 <igaojin@qq.com>
2021-03-24 20:27:48 +0800

    Update susd.js

--- js/susd.js
+++ js/susd.js
@@ -284,16 +284,17 @@ const run = async () => {
   const receiverLockScript = addressToScript(toAddress)
   console.log("receiverLockScript:",receiverLockScript)
 
-  const receiverCell = await account.getOtherCells(receiverLockScript)
-  console.log("receiverCell:",receiverCell);
+  const allreceiverCell = await account.getOtherCells(receiverLockScript)
 
   /* transfer */
-// const receiverCell = cells.find(cell => !cell.type && cell.data === '0x')
-// if (!receiverCell) {
-//   throw new Error('Please add a secp256k1 cell to receive sudt')
-// }
-const txHash = await account.transfer(null, 999n * BigInt(10 ** 8), receiverLockScript)
-console.log(txHash)
+  const receiverCell = allreceiverCell.find(cell => !cell.type && cell.data === '0x')
+  if (!receiverCell) {
+    throw new Error('Please add a secp256k1 cell to receive sudt')
+  }
+  console.log("receiverCell:",receiverCell);
+
+  const txHash = await account.transfer(null, 999n * BigInt(10 ** 8), receiverLockScript)
+  console.log(txHash)
 }
 
 run()
